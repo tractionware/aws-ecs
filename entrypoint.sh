6cd2d1c0081c149700094b9713f6ce4947fb7ba7
David Staheli <dastahel@microsoft.com>
2019-06-19 06:30:26 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e -u -o pipefail
 echo "Installing the AWS CLI..."
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
-sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 
 echo "Deploying..."
 echo -e "[appservices]\naws_access_key_id = ${AWS_ECS_TASK_DEFINITION}\naws_secret_access_key = ${AWS_ECS_TASK_DEFINITION}\n" > ~/.aws
